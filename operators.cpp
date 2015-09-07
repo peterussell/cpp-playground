39e53159d9ac0f32e1e2400981b5f6f8cc51abdb
Pete Russell <pinkbatz@gmail.com>
2015-09-07 00:15:30 -0700

    Added example for basic arithmetic operators

---
+++ operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // Demonstrates the basic arithmetic operators
+  int m = 54;
+  int n = 20;
+
+  cout << "m + n = " << m+n << endl;
+  cout << "m - n = " << m-n << endl;
+  cout << "m * n = " << m*n << endl;
+  cout << "m / n = " << m/n << endl;
+  cout << "m % n = " << m%n << endl;
+
+  cout << endl;
+}
