557c3aac1c8c9671ee6293b41c07c6f03919b5d3
Pete Russell <pinkbatz@gmail.com>
2015-09-06 23:02:15 -0700

    Added chars example from ch. 2. Updated gitignore for .swp files

---
+++ chars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char c1 = 'A';
+  cout << "c1 = " << c1 << ",\t\tint(c1) = " << int( c1 ) << endl;
+
+  char c2 = 't';
+  cout << "c2 = " << c2 << ",\t\tint(c2) = " << int( c2 ) << endl;
+
+  char c3 = '\t';
+  cout << "c3 = " << c3 << ",\tint(c3) = " << int( c3 ) << endl;
+
+  char c4 = '!';
+  cout << "c4 = " << c4 << ",\t\tint(c4) = " << int( c4 ) << endl;
+}
