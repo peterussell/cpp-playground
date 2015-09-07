1abb3fa5083720936d8725e37c08d2b8b77de585
Pete Russell <pinkbatz@gmail.com>
2015-09-06 22:03:59 -0700

    Initial commit

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int m, n;
+  cout << "Enter two integers: ";
+  cin >> m >> n;
+
+  cout << "m = " << m << endl;
+  cout << "n = " << n << endl;
+}
